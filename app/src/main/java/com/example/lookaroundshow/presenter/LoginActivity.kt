cd2f33fc68646bb9585de1f7bf3b1ca717799863
jaydks <jjeee1313@gmail.com>
2023-12-19 20:11:30 +0900

    [ui] 로그인 화면

--- app/src/main/java/com/example/lookaroundshow/presenter/LoginActivity.kt
+++ app/src/main/java/com/example/lookaroundshow/presenter/LoginActivity.kt
@@ -3,11 +3,17 @@ package com.example.lookaroundshow.presenter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lookaroundshow.R
+import com.example.lookaroundshow.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_LookAroundShow)
-        setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
+
